a5c73edf1b20c7014ac06c5fbb89962196f70d29
pedro0072005 <146725188+pedro0072005@users.noreply.github.com>
2023-11-06 08:56:30 -0300

    Add files via upload

---
+++ Mesa-sons-main/main.js
@@ -0,0 +1,37 @@
+// Vinicius Ribeiro da Silva 23
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio =  `#som_${instrumento}`; //template string
+    // console.log(instrumento);
+    // console.log(idAudio)
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    } 
+
+    tecla.onkeydown = function (evento) {
+
+        
+        console.log(evento)
+
+        if ('Espaço') {
+            tecla.classList.add('ativa')
+        } else {
+            tecla.classList.remove('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+
+}
